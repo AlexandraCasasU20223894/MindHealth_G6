6143cd242516274bddc9e32cec8fc0d3457eed0c
AlexandraCasasU20223894 <U202223894@upc.edu.pe>
2026-05-09 12:48:21 -0500

    feat: initial project structure and complete database schema for Sprint 1

---
+++ mind-health/src/main/java/com/upc/mind_health/MindHealthApplication.java
@@ -0,0 +1,13 @@
+package com.upc.mind_health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindHealthApplication.class, args);
+	}
+
+}
---
+++ mind-health/src/test/java/com/upc/mind_health/MindHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.mind_health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
